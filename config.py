5bcea5081e5cf85d72b47869b4fafa81242a3577
Neoikiru <andrey31bond@gmail.com>
2023-05-14 13:33:41 +0300

    Changed config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ ACTIVATION_WORD = 'Hey, Elli'  # [Start; Hey, Elli]
 GPT_TOKEN = ''
 PICOVOICE_ACCESS_KEY = ''
 ELEVENLABS_API_KEY = '' 
-EDU_PASSWORD = ''
-EDU_LOGIN = ''
 # Settings ---------------------------------------------------------
 
 # DO NOT TOUCH -----------------------------------------------------
+EDU_PASSWORD = ''
+EDU_LOGIN = ''
 LANGUAGE = 'English'  # [English; Ukrainian]
 Time_to_listen_after_request = 7
 STOP_WORDS = ['exit', 'вихід', 'stop']
